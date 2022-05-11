3c1444c56c490a9add0ee816a5c327f88d8c5cb8
Javernus <40899982+Javernus@users.noreply.github.com>
2022-05-11 18:50:41 +0200

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -11,7 +11,7 @@ rm -f ./Outputs/CPU/*
 rm -f ./Outputs/Graph/*
 rm -f ./Outputs/GData/*
 
-Suffix=MemEffV1
+Suffix=AddSuffixHere
 
 # Running the scheduler for each of the workloads.
 for MEM in {1..9} ; do
@@ -24,8 +24,8 @@ for MEM in {1..9} ; do
 
       # Running the scheduler. Tweak the range to change the number of iterations.
       for j in {1..15} ; do
-        # This is the command you run. Tweak it to what you are testing.
-        ./skeleton -c 0.${CPU} -i 0.${IO} -m 0.${MEM} -p 10000 -n 50 -t 4 -v 0 > Outputs/loop.txt
+        # TODO: This is the command you run. Tweak it to what you are testing.
+        ./skeleton -c 0.${CPU} -i 0.${IO} -m 0.${MEM} -p 10000 > Outputs/loop.txt
 
         # Reading the output of the scheduler and storing important data in an output file.
         while read -r line ;
@@ -78,22 +78,22 @@ for MEM in {1..9} ; do
         fi
       done < "Outputs/Graph/UtilDataC${CPU}_I${IO}_M${MEM}.txt"
 
-      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/waitmem-mean-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/WAITMEM-mean-${Suffix}.txt
-      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/waitmem-spread-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/WAITMEM-spread-${Suffix}.txt
-      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/waitmem-minimum-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/WAITMEM-min-${Suffix}.txt
-      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/waitmem-maximum-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/WAITMEM-max-${Suffix}.txt
-      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/waitcpu-mean-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/WAITCPU-mean-${Suffix}.txt
-      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/waitcpu-spread-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/WAITCPU-spread-${Suffix}.txt
-      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/waitcpu-minimum-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/WAITCPU-min-${Suffix}.txt
-      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/waitcpu-maximum-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/WAITCPU-max-${Suffix}.txt
-      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/execmem-mean-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/EXECMEM-mean-${Suffix}.txt
-      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/execmem-spread-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/EXECMEM-spread-${Suffix}.txt
-      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/execmem-minimum-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/EXECMEM-min-${Suffix}.txt
-      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/execmem-maximum-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/EXECMEM-max-${Suffix}.txt
-      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/exectime-mean-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/EXECTIME-mean-${Suffix}.txt
-      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/exectime-spread-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/EXECTIME-spread-${Suffix}.txt
-      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/exectime-minimum-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/EXECTIME-min-${Suffix}.txt
-      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/exectime-maximum-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/EXECTIME-max-${Suffix}.txt
+      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/waitmem-mean-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/waitmem-mean-${Suffix}.txt
+      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/waitmem-spread-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/waitmem-spread-${Suffix}.txt
+      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/waitmem-minimum-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/waitmem-min-${Suffix}.txt
+      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/waitmem-maximum-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/waitmem-max-${Suffix}.txt
+      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/waitcpu-mean-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/waitcpu-mean-${Suffix}.txt
+      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/waitcpu-spread-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/waitcpu-spread-${Suffix}.txt
+      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/waitcpu-minimum-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/waitcpu-min-${Suffix}.txt
+      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/waitcpu-maximum-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/waitcpu-max-${Suffix}.txt
+      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/execmem-mean-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/execmem-mean-${Suffix}.txt
+      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/execmem-spread-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/execmem-spread-${Suffix}.txt
+      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/execmem-minimum-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/execmem-min-${Suffix}.txt
+      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/execmem-maximum-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/execmem-max-${Suffix}.txt
+      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/exectime-mean-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/exectime-mean-${Suffix}.txt
+      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/exectime-spread-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/exectime-spread-${Suffix}.txt
+      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/exectime-minimum-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/exectime-min-${Suffix}.txt
+      echo CPU: 0.${CPU}, IO: 0.${IO}, MEM: 0.${MEM}. $(datamash -s mean 1 < "Outputs/GData/exectime-maximum-C${CPU}_I${IO}_M${MEM}.txt") >> Outputs/GData/exectime-max-${Suffix}.txt
     done
   done
 done
