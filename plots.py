138a6ca1a3125c8cf76284a937fe33ffcac2c76a
Javernus <40899982+Javernus@users.noreply.github.com>
2022-05-11 18:53:57 +0200

    Update plots.py

--- plots.py
+++ plots.py
@@ -10,9 +10,6 @@ def plot_schedule(zLabel, schedule, filenames, plotnames):
     # Create the figure.
     fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(111, projection='3d')
-    # ax.set_xlim(0, 1)
-    # ax.set_ylim(0, 1)
-    # ax.set_zlim(0, 1)
     ax.set_xlabel('CPU Workload')
     ax.set_ylabel('Memory Workload')
     ax.set_zlabel(zLabel)
@@ -21,6 +18,7 @@ def plot_schedule(zLabel, schedule, filenames, plotnames):
     marker = ['o', 'x', '+', '^', 'v', '<', '>']
     i = 0
     for filename in filenames:
+        print('Plotting:', filename)
         data = np.loadtxt(filename)
 
         # Plot the data.
@@ -37,14 +35,13 @@ def plot_schedule(zLabel, schedule, filenames, plotnames):
     ax.view_init(20, -75)
 
     # Save the figure.
-    fig.savefig(schedule + '.png', dpi=600)
+    fig.savefig(schedule + '.png', dpi=1200, bbox_inches='tight')
     plt.close(fig)
 
 
-plot_schedule('CPU Utilisation', 'cpu_util', [
-              './Outputs/Diff/CPU-Util-Matrix-0.txt', './Outputs/Diff/CPU-Util-Matrix-1.txt', './Outputs/Diff/CPU-Util-Matrix-2.txt', './Outputs/Diff/CPU-Util-Matrix-3.txt', './Outputs/Diff/CPU-Util-Matrix-4.txt', './Outputs/Diff/CPU-Util-Matrix-4.txt'], ['Round Robin', 'Advanced V1', 'Advanced V2', 'Advanced V3', 'Advanced V4', 'Advanced V5'])
-plot_schedule('Memory Utilisation', 'mem_util', [
-              './Outputs/Diff/MEM-Util-Matrix-0.txt', './Outputs/Diff/MEM-Util-Matrix-1.txt', './Outputs/Diff/MEM-Util-Matrix-2.txt', './Outputs/Diff/MEM-Util-Matrix-3.txt', './Outputs/Diff/MEM-Util-Matrix-4.txt', './Outputs/Diff/MEM-Util-Matrix-5.txt'], ['Round Robin', 'Advanced V1', 'Advanced V2', 'Advanced V3', 'Advanced V4', 'Advanced V5'])
+plot_schedule('CPU Utilisation', 'cpu_util', ['./Outputs/Matrices/CPU-Util-Matrix-1.txt', './Outputs/Matrices/CPU-Util-Matrix-2.txt', './Outputs/Matrices/CPU-Util-Matrix-3.txt'], ['First', 'Second', 'Third'])
+plot_schedule('Memory Utilisation', 'mem_util', ['./Outputs/Matrices/MEM-Util-Matrix-1.txt', './Outputs/Matrices/MEM-Util-Matrix-2.txt',
+              './Outputs/Matrices/MEM-Util-Matrix-3.txt'], ['First', 'Second', 'Third'])
 
 graph_names = ["Time until Memory Allocation", "Time until Execution",
                "Time until Execution after Mem Alloc", "Execution Time"]
@@ -54,4 +51,6 @@ graph_types = ["mean", "spread", "min", "max"]
 for i in range(len(graph_data)):
     for j in range(len(graph_types)):
         plot_schedule(graph_names[i] + ' ' + graph_types[j], graph_data[i] + '-' + graph_types[j], ['./Outputs/Matrices/' +
-                      graph_data[i] + '-' + graph_types[j] + "-Matrix-6.txt"], ["Advanced V5"])
+                      graph_data[i] + '-' + graph_types[j] + "-Matrix-CompIDSuffix1.txt", './Outputs/Matrices/' +
+                      graph_data[i] + '-' + graph_types[j] + "-Matrix-CompIDSuffix2.txt", './Outputs/Matrices/' +
+                      graph_data[i] + '-' + graph_types[j] + "-Matrix-CompIDSuffix3.txt"], ["First Scheduler Name", "Second", "Third"])
